2a757ee3b857c667f50072dbfbcebcfb3abcfa72
Maritxx <maritdevries1@live.nl>
2022-08-10 18:46:34 +0200

    Created object.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function drawBoard() {
-	const gameElement = document.querySelector('.game')
+const state = { //Creates object that you re-use.
+	gameElement: document.querySelector('.game')
+	cells: Array(9).fill(null) 								//Array function takes length parameter so you don't need to re-type code.
+}
 
+
+function drawBoard() {
 	for (let i = 0; i < 9; i++) {
 		const cellContainer = document.createElement('div')
 		cellContainer.classList.add('cell')
