350ade76327ae9adaedc128718718653e43968e8
Maritxx <maritdevries1@live.nl>
2022-08-10 19:17:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,21 +12,24 @@ function drawBoard() {
 	console.log("DrawBoard has run");
 	state.gameElement.innerHTML = ''
 
-	for (let i = 0; i < 9; i++) {
+	for (let i = 0; i < state.cells.length; i++) {
 		const cellContainer = document.createElement('div')
 		cellContainer.classList.add('cell')
 
 
 		if (state.cells[i]) { //Does the cell have something in it? Then the code runs.
 			const cellSymbol = document.createElement('p')
-			cellSymbol.innerText = 'x'
+			cellSymbol.innerText = state.cells[i]
 			cellSymbol.classList.add('symbol')
 
 			cellContainer.append(cellSymbol)
 		} else { //If not, this code runs. 
 			cellContainer.addEventListener('click', function () {
 				state.symbols.reverse()
-				state.cells[i] = state.symbols[0]
+				const currentPlayerSymbol = state.symbols[0]
+
+				state.cells[i] = currentPlayerSymbol
+
 
 				drawBoard() 
 				//Re-draws the grid again after something is clicked.
