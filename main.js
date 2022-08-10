82dd21a3083a0ab89801f09998bb0a2d3a937616
Maritxx <maritdevries1@live.nl>
2022-08-10 18:33:40 +0200

    Code creates cells.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function drawBoard() {
 	const gameElement = document.querySelector('.game')
 
-	const cellContainer = document.createElement('div')
-	cellContainer.classList.add('cell')
+	for (let i = 0; i < 9; i++) {
+		const cellContainer = document.createElement('div')
+		cellContainer.classList.add('cell')
 
-	gameElement.append(cellContainer)
+		gameElement.append(cellContainer)
+	}
 }
+
+drawBoard();
